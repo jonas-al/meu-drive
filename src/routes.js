3a166f0fdb0e07d2d8955dd7f5906de2c50b46de
Jonas Alberto <yonas.alluniverse@gmail.com>
2023-12-02 22:14:03 -0300

    feat: roteamento

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Ionicons } from '@expo/vector-icons'
 
 // Pages
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
+import FileList from './pages/FileList/FileList'
 
 
 const Tab = createBottomTabNavigator()
@@ -20,15 +21,39 @@ export const Routes = () => {
             if (focused) {
               return <Ionicons
                 size={size} 
-                color={color}
+                color='#0C55A5'
                 name='cloud-upload'
               />
             }
 
             return <Ionicons
               size={size} 
-              color={color}
+              color='#0C55A5'
               name='cloud-upload-outline'
+            />
+          }
+        }}
+      />
+
+      <Tab.Screen
+        name='file list'
+        component={FileList}
+        options={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused) {
+              return <Ionicons
+                size={size} 
+                color='#0C55A5'
+                name='file-tray-full'
+              />
+            }
+
+            return <Ionicons
+              size={size} 
+              color='#0C55A5'
+              name='file-tray-full-outline'
             />
           }
         }}
