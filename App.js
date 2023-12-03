456fa648cd67e47a31dd38344b3369eca6ae20c4
Jonas Alberto <yonas.alluniverse@gmail.com>
2023-12-02 22:13:34 -0300

    feat: novo tema default

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { useFonts } from 'expo-font';
-import { TamaguiProvider } from 'tamagui'
+import { TamaguiProvider, Theme } from 'tamagui'
+import { useColorScheme } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { Routes } from './src/routes';
 
 import config from './tamagui.config';
 
 export default function App() {
+  const colorScheme = useColorScheme();
+
   const [loaded] = useFonts({
     Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
     InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
@@ -17,9 +20,11 @@ export default function App() {
 
   return (
     <TamaguiProvider config={config}>
-      <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
+      <Theme name='blue'>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </Theme>
     </TamaguiProvider>
   );
 }
